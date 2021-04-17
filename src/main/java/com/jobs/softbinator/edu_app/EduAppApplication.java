4f64a4529d9dd57f04b390111b2b096339ee5dab
alexmihailescu99 <alexmihailescu99@gmail.com>
2021-04-17 16:48:17 +0300

    Initial commit

---
+++ src/main/java/com/jobs/softbinator/edu_app/EduAppApplication.java
@@ -0,0 +1,13 @@
+package com.jobs.softbinator.edu_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobs/softbinator/edu_app/EduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.softbinator.edu_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
